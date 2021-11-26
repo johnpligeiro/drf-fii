cdfd53fd41098bc032b823e90b9ec578bd69fd4b
johnpligeiro <johnpligeiro@gmail.com>
2021-11-25 23:24:44 -0300

    add ordering filters

--- fundsfinder/api/views.py
+++ fundsfinder/api/views.py
@@ -5,7 +5,7 @@
 # Agora se você precisar construir uma API “com tudo dentro” (isto é: criação, deleção, atualização e listagem), escolha a ModelViewSet: perceba que ela estende todos os Mixins disponíveis.
 
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from api.serializers import FundoImobiliarioSerializer
 from rest_framework import viewsets, permissions
 from api.models import FundoImobiliario
@@ -15,9 +15,17 @@ from api.models import FundoImobiliario
 
 
 class FundoImobiliarioViewSet(viewsets.ModelViewSet):
-  queryset = FundoImobiliario.objects.all()
-  serializer_class = FundoImobiliarioSerializer
-  permission_classes = [permissions.IsAuthenticated]
-  filter_backends = [DjangoFilterBackend, SearchFilter]
-  filterset_fields = ['codigo', 'setor']
-  search_fields = ['codigo', 'setor']
+    queryset = FundoImobiliario.objects.all()
+    serializer_class = FundoImobiliarioSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['codigo', 'setor']
+    search_fields = ['codigo', 'setor']
+    ordering = ['-dividend_yield_medio_12m']
+    ordering_fields = [
+        'dividend_yield_medio_12m',
+        'setor',
+        'vacancia_financeira',
+        'vacancia_fisica',
+        'quantidade_ativos']
+
